02e4623f74f872e61c1a552f2cd8772c4e22431e
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-12-31 00:18:25 +0100

    finish slideshow component

--- blog/.vuepress/components/Slideshow.vue
+++ blog/.vuepress/components/Slideshow.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <img :src="images[slideIndex]" />
-        <div><span @click="updateSlideIndex(-1)">Previous</span><span @click="updateSlideIndex(1)">Next</span></div>
+        <div style="display: flex; justify-content: center">
+            <span :class="{ 'disabled-slider': slideIndex === 0 }" class="green-underline-hover" style="margin-right: 10px; cursor: pointer" @click="updateSlideIndex(-1)">Previous</span>
+            <span :class="{ 'disabled-slider': slideIndex === images.length - 1 }" class="green-underline-hover" style="cursor:pointer" @click="updateSlideIndex(1)">Next</span>
+        </div>
     </div>
 </template>
 
 <script>
-
 export default {
     props: {
         images: {
@@ -21,8 +23,26 @@ export default {
     },
     methods: {
         updateSlideIndex(i) {
+            if (i === -1 && this.slideIndex === 0) {
+                return;
+            } else if (i === 1 && this.slideIndex === this.images.length - 1) {
+                return;
+            } 
             this.slideIndex += i;
         }
     }
 }
 </script>
+
+<style lang="stylus">
+    .green-underline-hover:hover
+        text-decoration underline
+        text-decoration-color $accentColor
+        text-decoration-thickness 2px
+        text-underline-offset 2px
+
+    .disabled-slider
+        color lightness($textColor, 60%) !important
+        text-decoration none !important
+        cursor default !important
+</style>
